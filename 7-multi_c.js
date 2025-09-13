6dd906216b6521e9277ff419c7f6389e973664d5
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-09-13 22:30:38 +0200

    Looped according to argument entered

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+let line = "C is fun";
+let argument = process.argv[2];
+if(typeof argument !== "number"){
+	console.log("Missing number of occurences");
+}
+else{
+	let count = 0;
+	while(count < argument){
+		console.log(line);
+		count++;
+	}
+}
