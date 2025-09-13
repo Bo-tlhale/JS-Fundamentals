065f40f5b6894b79972786d85e18fa71e5517d86
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-09-13 22:22:24 +0200

    Update array sentence

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 
-let arr = ["C is fun ", "Python is cool", "JavaScript is amazing"];
+let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
 arr.forEach(line => {
 	console.log(line);
 });
